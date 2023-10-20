6b24449046046aec1ff21ea622623c978d471304
Yusuf Taha Kara <yusuftahakara@gmail.com>
2023-10-20 17:15:22 +0300

    firebase getProduct process completed

--- Angular/ng-app/src/app/product-list/product-list.component.ts
+++ Angular/ng-app/src/app/product-list/product-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../models/product';
 import { ProductRepository } from '../models/product.repository';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'product-list',
@@ -9,10 +10,10 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
-  products: Product[];
+  products: Product[] = [];
   productRepository: ProductRepository;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private http: HttpClient) {
     // active bir route gerekli bu yüzden bunu buraya yazıyoruz.
     this.productRepository = new ProductRepository();
   }
@@ -25,7 +26,15 @@ export class ProductListComponent implements OnInit {
           params['categoryId']
         );
       } else {
-        this.products = this.productRepository.getProducts();
+        this.http
+          .get<Product[]>( // dönece olan nesnenin tipini belirtebiliyorsun.
+            `https://ng-shoppapp-a4857-default-rtdb.firebaseio.com/product.json`
+          )
+          .subscribe((result) => {
+            for (let key in result) {
+              this.products.push({ ...result[key], id: key });
+            }
+          });
       }
     });
   }
