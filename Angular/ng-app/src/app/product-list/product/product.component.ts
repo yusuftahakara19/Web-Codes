c4d48be25f16ba71f5fbc2575a15b982e96183bb
Yusuf Taha Kara <yusuftahakara@gmail.com>
2023-10-22 14:32:53 +0300

    product-details service added

--- Angular/ng-app/src/app/product-list/product/product.component.ts
+++ Angular/ng-app/src/app/product-list/product/product.component.ts
@@ -1,25 +1,29 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/models/product';
-import { ProductRepository } from 'src/app/models/product.repository';
+
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css'],
+  providers: [ProductService],
 })
 export class ProductComponent implements OnInit {
   product: Product | undefined;
-  productRepository: ProductRepository;
 
-  constructor(private route: ActivatedRoute) {
-    this.productRepository = new ProductRepository();
-  }
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {}
 
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
       const id = params['productId'];
-      this.product = this.productRepository.getProductById(id);
+      this.productService.getProductById(id).subscribe((result) => {
+        this.product = { ...result, id: id };
+      });
     });
   }
 }
--- Angular/ng-app/src/app/services/product.service.ts
+++ Angular/ng-app/src/app/services/product.service.ts
@@ -6,16 +6,20 @@ import { Observable } from 'rxjs';
 //local service
 @Injectable()
 export class ProductService {
-  private url = `https://ng-shoppapp-a4857-default-rtdb.firebaseio.com/product.json`;
+  private url = `https://ng-shoppapp-a4857-default-rtdb.firebaseio.com/`;
 
   //constructor'da bir http nesnesini inject edeceğiz.
   constructor(private http: HttpClient) {}
 
   getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.url);
+    return this.http.get<Product[]>(this.url + 'product.json');
+  }
+
+  getProductById(id: string): Observable<Product> {
+    return this.http.get<Product>(this.url + 'product/' + id + '.json');
   }
 
   createProduct(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.url, product);
+    return this.http.post<Product>(this.url + 'product.json', product);
   }
 }
