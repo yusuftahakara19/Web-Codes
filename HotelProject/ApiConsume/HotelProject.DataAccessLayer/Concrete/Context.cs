6af8206a28be4d47060647b41c879782da93d0f0
Yusuf Taha Kara <tkyusuft@architecht.com>
2023-12-05 17:22:35 +0300

    hotel project 5.12.23

---
+++ HotelProject/ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,25 @@
+﻿using HotelProject.EntityLayer;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=ARC166NB\\SQLEXPRESS;initial catalog=ApiDb;integrated security=true");
+         }
+        
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Staff> Staffs { get; set; }
+        public DbSet<Subscribe> Subscribes { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
