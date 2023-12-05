a720f8592eb5f7d5820bfcaf1c6489baea3cf235
Yusuf Taha Kara <yusuftahakara@gmail.com>
2023-12-05 09:33:45 +0300

    hotel project started. Asp.Net Core Api - Rapid api and api consume

---
+++ HotelProject/ApiConsume/HotelProject.EntityLayer/Testimonial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.EntityLayer
+{
+    public class Testimonial
+    {
+        public int TestimonialID { get; set; }
+        public string Name { get; set; }
+        public int Title{ get; set; }
+        public int Description{ get; set; }
+        public int Image{ get; set; }
+    }
+}
