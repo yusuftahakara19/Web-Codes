7f16fa6ccfa32912d2bb324569e812802516ecd0
yusuftahakara19 <yusuftahakara@gmail.com>
2024-03-06 11:05:41 +0300

    AddDependencies, Respositories and Uow added

--- Asp.Net Core Mvc-Api/Udemy.ToDoAppNTier/Udemy.ToDoAppNTier.UI/Startup.cs
+++ Asp.Net Core Mvc-Api/Udemy.ToDoAppNTier/Udemy.ToDoAppNTier.UI/Startup.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Udemy.TodoAppNTier.Business.DependencyResolvers.Microsoft;
 
 namespace Udemy.ToDoAppNTier.UI
 {
@@ -16,6 +17,7 @@ namespace Udemy.ToDoAppNTier.UI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDependencies();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ Asp.Net Core Mvc-Api/Udemy.ToDoAppNTier/Udemy.TodoAppNTier.Business/DependencyResolvers/Microsoft/DependencyExtension.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Udemy.TodoAppNTier.DataAccess.Contexts;
+
+namespace Udemy.TodoAppNTier.Business.DependencyResolvers.Microsoft
+{
+    public static class DependencyExtension
+    {
+        public static void AddDependencies(this IServiceCollection services)
+        {
+            services.AddDbContext<TodoContext>(opt =>
+            {
+                opt.UseSqlServer("server=ARC166NB\\SQLEXPRESS; database=TodoDb; integrated security=true;");
+            });
+        }
+
+    }
+}
