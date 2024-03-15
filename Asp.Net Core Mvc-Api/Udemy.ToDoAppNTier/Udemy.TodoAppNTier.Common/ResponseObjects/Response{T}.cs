b42ac7777cac95713f446ff17058a4fa3991ee75
yusuftahakara19 <yusuftahakara@gmail.com>
2024-03-15 17:51:11 +0300

    ResponseObjects added

---
+++ Asp.Net Core Mvc-Api/Udemy.ToDoAppNTier/Udemy.TodoAppNTier.Common/ResponseObjects/Response{T}.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udemy.TodoAppNTier.Common.ResponseObjects
+{
+    public class Response<T>  : Response, IResponse<T>
+    {
+        public T Data { get; set; }
+        public List<CustomValidationError> ValidationErrors { get; set; }
+        public Response(ResponseType responseType, string message) : base(responseType,message)
+        {}
+        public Response(ResponseType responseType,T data,List<CustomValidationError> errors) : base(responseType)
+        {
+            Data = data;
+            ValidationErrors = errors;  
+        }
+        public Response(ResponseType responseType, T data) : base(responseType)
+        {
+            Data = data;
+        }
+    }
+}
